7370bf58512699c31fbf1d01099f31683fe5bb40
Wei La <weila@users.noreply.github.com>
2016-09-06 11:53:40 +0800

    minify js again

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+gulp.task('minifyjs', function() {
+    return gulp.src('js/*.js')
+        .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
+        .pipe(uglify())    //压缩
+        .pipe(gulp.dest('js/min'));  //输出
+});
+
+gulp.task('clean', function(cb) {
+    del('js/min', cb)
+});
